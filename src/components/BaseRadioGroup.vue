768b1c248ee94c3094312c9e45a1248e12966cec
fleurdekroon <fleurdekroon@hotmail.com>
2022-10-06 15:18:25 +0200

    end of lesson 7

---
+++ src/components/BaseRadioGroup.vue
@@ -0,0 +1,47 @@
+<template>
+  <component
+    :is="vertical ? 'div' : 'span'"
+    v-for="option in options"
+    :key="option.value"
+    :class="{
+      horizontal: !vertical
+    }"
+  >
+    <BaseRadio
+      :label="option.label"
+      :value="option.value"
+      :name="name"
+      :modelValue="modelValue"
+      @update:modelValue="$emit('update:modelValue', $event)"
+    />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    modelValue: {
+      type: [String, Number],
+      required: true
+    },
+    vertical: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .horizontal {
+    margin-right: 20px;
+  }
+</style>
